15e7b1ba7b665eeeb7f250ef65cb19a3a456050b
alexandre <akekiwy@gmail.com>
2020-02-14 15:20:07 +1100

    write assignation in db fix

--- app/src/main/java/com/example/cleanapp/LoginActivity.java
+++ app/src/main/java/com/example/cleanapp/LoginActivity.java
@@ -172,7 +172,6 @@ public class LoginActivity extends AppCompatActivity {
 
     void verifyDialog(){
         FirebaseUser user = myFirebaseAuth.getCurrentUser();
-        FirebaseAuth.getInstance().signOut();
 
         AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
         builder.setTitle("Verify account")
